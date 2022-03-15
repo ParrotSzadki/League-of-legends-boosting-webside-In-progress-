0f462622567fdfc222d9e9f88855754397ad8d0f
ParrotSzadki <101201980+ParrotSzadki@users.noreply.github.com>
2022-03-15 10:33:04 +0100

    Update BDFB.php

--- BDFB.php
+++ BDFB.php
@@ -5,7 +5,6 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="BDFB.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script src="dropdown.js"></script>
 </head>
 <body>
   <header>
@@ -18,43 +17,54 @@
     <div class="grid-item move-right-smaller text-item">
        <h2>Current League</h2>
     <div class="ranks">
-       <button type="button" class="ranks-button">I</button>
-       <button type="button" class="ranks-button">B</button>
+       <button type="button" onclick="" class="ranks-button">
+         <img src="jpgs/iron.jpg" alt="iron" width="50px" height="50px">
+       </button>
+       <button type="button" onclick="" class="ranks-button">
+         <img src="jpgs/bronze.jpg" alt="bronze" width="50px" height="50px">
+       </button>
     </div>
     <div class="ranks">
-       <button type="button" class="ranks-button">S</button>
-       <button type="button" class="ranks-button">G</button>
+      <button type="button" onclick="" class="ranks-button">
+        <img src="jpgs/silver.jpg" alt="silver" width="50px" height="50px">
+      </button>
+      <button type="button" onclick="" class="ranks-button">
+        <img src="jpgs/gold.jpg" alt="gold" width="50px" height="50px">
+      </button>
     </div>
     <div class="ranks">
-       <button type="button" class="ranks-button">P</button>
-       <button type="button" class="ranks-button">D</button>
+      <button type="button" onclick="" class="ranks-button">
+        <img src="jpgs/platinium.jpg" alt="platinium" width="50px" height="50px">
+      </button>
+      <button type="button" onclick="" class="ranks-button">
+        <img src="jpgs/diamond.jpg" alt="diamond" width="50px" height="50px">
+      </button>
     </div>
     <div class="ranks">
-       <button type="button" class="ranks-button">M</button>
+      <button type="button" onclick="" class="ranks-button">
+        <img src="jpgs/master.jpg" alt="master" width="50px" height="50px">
+      </button>
     </div>
-  <div class="ranks">
-    <div class="dropdown">
-      <button class="dropbtn">Curret lp</button>
-        <div class="dropdown-content">
-          <a href="#">0-20 lp</a>
-          <a href="#">21-40 lp</a>
-          <a href="#">41-60 lp</a>
-          <a href="#">61-60 lp</a>
-          <a href="#">81-99 lp</a>
-        </div>
-      </div>
-    <select class="button" name="lp-gains">
+    <div class="ranks">
+      <select class="current-button" name="lp-current">
+        <option value="020">0-20 lp</option>
+        <option value="2140">21-40 lp</option>
+        <option value="4160">41-60 lp</option>
+        <option value="6180">61-80 lp</option>
+        <option value="8199">81-99 lp</option>
+      </select>
+
+    <select class="ranks-button" name="lp-gains">
       <option value="11+">11 or more</option>
       <option value="13+">13 or more</option>
       <option value="15+">15 or more</option>
       <option value="17+">17 or more</option>
       <option value="20+">20 or more</option>
     </select>
-        </div>
+    </div>
     </div>
   <div class="grid-item move-right">2</div>
   </div>
-</div>
   <div class="grid-container">
     <div class="grid-item move-right-smaller">3</div>
   </div>
