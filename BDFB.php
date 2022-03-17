f592adccbeb063873b40ea127b460fe91663bb4e
ParrotSzadki <101201980+ParrotSzadki@users.noreply.github.com>
2022-03-17 11:08:06 +0100

    Update BDFB.php

--- BDFB.php
+++ BDFB.php
@@ -17,18 +17,18 @@
     <div class="grid-item move-right-smaller text-item">
        <h2>Current League</h2>
     <div class="ranks">
-       <button type="button" onclick="" class="ranks-button">
+       <button id="ironButton1" type="button" onclick="change()" class="ranks-button">
          <img src="jpgs/iron.jpg" alt="iron" width="50px" height="50px">
        </button>
-       <button type="button" onclick="" class="ranks-button">
+       <button id="btn2" type="button" onclick="pictureChange()" class="ranks-button">
          <img src="jpgs/bronze.jpg" alt="bronze" width="50px" height="50px">
        </button>
     </div>
     <div class="ranks">
-      <button type="button" onclick="" class="ranks-button">
+      <button id="btn3" type="button" onclick="pictureChange()" class="ranks-button">
         <img src="jpgs/silver.jpg" alt="silver" width="50px" height="50px">
       </button>
-      <button type="button" onclick="" class="ranks-button">
+      <button type="button" onclick="pictureChange()" class="ranks-button">
         <img src="jpgs/gold.jpg" alt="gold" width="50px" height="50px">
       </button>
     </div>
@@ -41,7 +41,7 @@
       </button>
     </div>
     <div class="ranks">
-      <button type="button" onclick="" class="ranks-button">
+      <button id="btn1" type="button" onclick="" class="ranks-button">
         <img src="jpgs/master.jpg" alt="master" width="50px" height="50px">
       </button>
     </div>
@@ -53,23 +53,73 @@
         <option value="6180">61-80 lp</option>
         <option value="8199">81-99 lp</option>
       </select>
-
-    <select class="ranks-button" name="lp-gains">
-      <option value="11+">11 or more</option>
-      <option value="13+">13 or more</option>
-      <option value="15+">15 or more</option>
-      <option value="17+">17 or more</option>
-      <option value="20+">20 or more</option>
-    </select>
+      <select class="ranks-button" name="lp-gains">
+        <option value="11+">11 or more</option>
+        <option value="13+">13 or more</option>
+        <option value="15+">15 or more</option>
+        <option value="17+">17 or more</option>
+        <option value="20+">20 or more</option>
+      </select>
     </div>
     </div>
-  <div class="grid-item move-right">2</div>
+  <div class="grid-item move-right text-item">
+    <h2>Checkout</h2>
+    <div class="ranks">
+      <img id="from" src="jpgs/silver.jpg" alt="silver" width="60px" height="60px">
+      <img id="to" src="jpgs/gold.jpg" alt="gold" width="60px" height="60px">
+    </div>
+  </div>
   </div>
   <div class="grid-container">
-    <div class="grid-item move-right-smaller">3</div>
+    <div class="grid-item move-right-smaller text-item">
+      <h2>Desired League</h2>
+      <div class="ranks">
+         <button type="button" onclick="" class="ranks-button">
+           <img src="jpgs/iron.jpg" alt="iron" width="50px" height="50px">
+         </button>
+         <button type="button" onclick="" class="ranks-button">
+           <img src="jpgs/bronze.jpg"  alt="bronze" width="50px" height="50px">
+         </button>
+      </div>
+      <div class="ranks">
+        <button type="button" onclick="" class="ranks-button">
+          <img src="jpgs/silver.jpg" alt="silver" width="50px" height="50px">
+        </button>
+        <button type="button" onclick="" class="ranks-button">
+          <img src="jpgs/gold.jpg" alt="gold" width="50px" height="50px">
+        </button>
+      </div>
+      <div class="ranks">
+        <button type="button" onclick="" class="ranks-button">
+          <img src="jpgs/platinium.jpg" alt="platinium" width="50px" height="50px">
+        </button>
+        <button type="button" onclick="" class="ranks-button">
+          <img src="jpgs/diamond.jpg" alt="diamond" width="50px" height="50px">
+        </button>
+      </div>
+      <div class="ranks">
+        <button type="button" onclick="" class="ranks-button">
+          <img src="jpgs/master.jpg" alt="master" width="50px" height="50px">
+        </button>
+      </div>
+      <div class="server">
+        <select class="current-button" name="server">
+          <option value="eune">Europe Nordic & East</option>
+          <option value="euw">Europe West</option>
+          <option value="ru">Russia</option>
+          <option value="tr">Turkey</option>
+        </select>
+      </div>
+      <div class="queue">
+        <input type="radio" class="radio" id="soloq" name="soloq" value="soloq" checked>
+        <label for="soloq">SoloQ</label>
+        <input type="radio" class="radio" id="soloq" name="soloq" value="soloq">
+        <label for="soloq">FlexQ</label>
+    </div>
   </div>
   </main>
   <footer>
   </footer>
+  <script src="BDFB.js"></script>
 </body>
 </html>
