ced6194255b3707e19c602202747aac7201de068
ParrotSzadki <101201980+ParrotSzadki@users.noreply.github.com>
2022-03-17 11:08:33 +0100

    Create BDFB.js

---
+++ BDFB.js
@@ -0,0 +1,20 @@
+
+function change(){
+  var image = document.getElementByID("from")
+  image.src = "jpgs/sulber.jpg"
+}
+
+/*
+let img = document.querySelector('img');
+ btn1.addEventListener('click', () => {
+  img.src = "jpgs/iron.jpg";
+})
+
+btn2.addEventListener('click', () => {
+  img.src = "jpgs/bronze.jpg";
+})
+
+btn3.addEventListener('click', () => {
+  img.src = "jpgs/silver.jpg";
+})
+*/
